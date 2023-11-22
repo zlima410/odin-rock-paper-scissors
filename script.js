99a5a784439c29e990f6fc4639ce54f74488c79a
zlima <endercraft68@gmail.com>
2023-11-22 00:38:03 -0600

    Created getComputerChoice function that randomly selects the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
 
+    switch (choice) {
+        case 1: 
+            console.log("Rock");
+            break;
+
+        case 2:
+            console.log("Paper");
+            break;
+
+        case 3:
+            console.log("Scissors");
+            break;
+    }
+}
