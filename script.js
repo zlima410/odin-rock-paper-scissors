a49aecbe9408e19deb8fb5c71f8456b6f813f5b0
zlima <endercraft68@gmail.com>
2023-11-22 00:54:44 -0600

    Created playRound function that evaluates the player and computer choices to decide a winner

--- script.js
+++ script.js
@@ -3,15 +3,40 @@ function getComputerChoice() {
 
     switch (choice) {
         case 1: 
-            console.log("Rock");
+            return "Rock";
             break;
 
         case 2:
-            console.log("Paper");
+            return "Paper";
             break;
 
         case 3:
-            console.log("Scissors");
+            return "Scissors";
             break;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock";
+
+    }
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    }
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+    else {
+        return "It's a tie!";
     }
 }
