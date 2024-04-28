6edf1fbf144709598367ab5e07866a6759293e36
zlima410 <endercraft68@gmail.com>
2024-04-27 22:40:26 -0400

    Refactor game logic to use event listeners and emojis for player and computer choices

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const selection = document.querySelector(".rps-select");
+const score = document.querySelector("h4");
+const player = document.querySelector("#player");
+const pc = document.querySelector("#computer");
+const currPlayerScore = document.querySelector("#player-score");
+currPlayerScore.textContent = playerScore;
+const currComputerScore = document.querySelector("#computer-score");
+currComputerScore.textContent = computerScore;
+
 function getComputerChoice() {
     //find a random number between 1 and 3
     let choice = Math.floor(Math.random() * 3) + 1;
@@ -5,93 +17,94 @@ function getComputerChoice() {
     //return the choice based on the random number
     switch (choice) {
         case 1: 
-            return "Rock";
-            break;
+            return (pc.textContent= "🪨");
 
         case 2:
-            return "Paper";
-            break;
+            return (pc.textContent = "📃");
 
         case 3:
-            return "Scissors";
+            return (pc.textContent = "✂️");
+    }
+}
+
+function playRound(player, pc) {
+    //check for all possible outcomes and return the result of both the player and computer selections
+    if (player === pc) {
+        score.textContent = "No points awarded.";
+        currPlayerScore.textContent = playerScore;
+        currComputerScore.textContent = computerScore;
+    } 
+
+    switch (player) {
+        case "🪨":
+            switch (pc) {
+                case "📃":
+                    computerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Lose! Paper beats Rock...";
+                    break;
+                case "✂️":
+                    playerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Win! Rock beats Scissors!";
+                    break;
+            }
+            break;
+
+        case "📃":
+            switch (pc) {
+                case "✂️":
+                    computerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Lose! Scissors beats Paper...";
+                    break;
+                case "🪨":
+                    playerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Win! Paper beats Rock!";
+                    break;
+            }
+            break;
+
+        case "✂️":
+            switch (pc) {
+                case "🪨":
+                    computerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Lose! Rock beats Scissors...";
+                    break;
+                case "📃":
+                    playerScore++;
+                    currPlayerScore.textContent = playerScore;
+                    currComputerScore.textContent = computerScore;
+                    score.textContent = "You Win! Scissors beats Paper!";
+                    break;
+            }
             break;
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    //check for all possible outcomes and return the result of both the player and computer selections
-    if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-        return "You Win! Rock beats Scissors";
+selection.addEventListener("click", (e) => {
+    let click = e.target;
+    switch (click.id) {
+        case "rock":
+            playRound("🪨", getComputerChoice());
+            player.textContent = "🪨";
+            break;
+        case "paper":
+            playRound("📃", getComputerChoice());
+            player.textContent = "📃";
+            break; 
+        case "scissors":
+            playRound("✂️", getComputerChoice());
+            player.textContent = "✂️";
+            break;
     }
-    else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock";
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-        return "You Win! Paper beats Rock";
-    }
-    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
-        return "You Lose! Scissors beats Paper";
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-        return "You Win! Scissors beats Paper";
-    }
-    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
-        return "You Lose! Rock beats Scissors";
-    }
-    else {
-        return "It's a tie!";
-    }
-}
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let computerSelection = getComputerChoice();
-
-    let option = document.querySelector("#option");
-    option.addEventListener('click', (event) => {
-        let target = event.target;
-
-        switch(target.id) {
-            case 'rock':
-                let result1 = playRound("Rock", computerSelection);
-                if (result1.includes("Win"))
-                    playerScore++;
-                else if (result1.includes("Lose"))
-                    computerScore++;
-                break;
-            case 'paper':
-                let result2 = playRound("Paper", computerSelection);
-                if (result2.includes("Win")) 
-                    playerScore++;
-                else if (result2.includes("Lose")) 
-                    computerScore++;
-                break;
-            case 'scissors':
-                let result3 = playRound("Scissors", computerSelection);
-                if (result3.includes("Win"))
-                    playerScore++;
-                else if (result3.includes("Lose"))
-                    computerScore++;
-                break;
-        }
-    });
-
-    let results = document.createElement("div");
-
-    if (playerScore > computerScore) {
-        results.textContent = "You win the game!";
-    }
-    else if (playerScore < computerScore) {
-        results.textContent = "You lose the game...";
-    }
-    else if (playerScore === computerScore && playerScore !== 0) {
-        results.textContent = "You guys have tied."
-    }
-    else {
-        results.textContent = "No game was played.";
-    }
-}
-
-game();
+    
+});
